2a9fa339fd08b984e2be5b022a3343b1b54e2c35
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2022-08-30 02:09:10 -0300

    Create Cards-subItems.vue

---
+++ FrontEnd/components/Cards-subItems.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="avatar">
+            <div class="w-24 rounded">
+              <img src="https://placeimg.com/192/192/people" />
+              <span>Title</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
